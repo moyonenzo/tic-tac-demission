5dbc4616e544d7a08f4530f5486dad829d8bb339
moyonenzo <moyon_e@etna-alternance.net>
2024-10-22 13:50:33 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { ModeWatcher } from "mode-watcher";
+
+    import { Separator } from "$lib/components/ui/separator";
+
+    import * as Avatar from "$lib/components/ui/avatar";
+    import * as Card from "$lib/components/ui/card";
+    import Counter from "$lib/Counter.svelte";
+</script>
+
+<ModeWatcher />
+
+<div class="flex h-screen justify-center items-center">
+    <Card.Root>
+        <Card.Content>
+
+            <div class="flex h-5 items-center space-x-4 text-2xl">
+
+                <Counter date={new Date("2024-11-05 17:00:00")} />
+
+                <Avatar.Root>
+                    <Avatar.Image src="https://trombine.etna.io/saconn_a/large" alt="@moyon_e" />
+                    <Avatar.Fallback>SA</Avatar.Fallback>
+                </Avatar.Root>
+
+                <Separator orientation="vertical" />
+
+                <Avatar.Root>
+                    <Avatar.Image src="https://trombine.etna.io/moyon_e/large" alt="@moyon_e" />
+                    <Avatar.Fallback>ME</Avatar.Fallback>
+                </Avatar.Root>
+
+                <Counter date={new Date("2024-12-31 12:00:00")} />
+            </div>
+
+
+        </Card.Content>
+    </Card.Root>
+</div>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let date: Date;
+
+    let days: number;
+    let hours: number;
+    let minutes: number;
+    let seconds: number;
+
+    function calculateRemaining() {
+        const now = new Date();
+        const sec = (date - now) / 1000;
+        const n   = 24 * 3600;
+
+        days = Math.floor (sec / n);
+        hours = Math.floor ((sec - (days * n)) / 3600);
+        minutes = Math.floor ((sec - ((days * n + hours * 3600))) / 60);
+        seconds = Math.floor (sec - ((days * n + hours * 3600 + minutes * 60)));
+
+        setTimeout(calculateRemaining, 1000)
+    }
+
+    calculateRemaining();
+</script>
+
+<p>
+    {days} :
+    {hours} :
+    {minutes} :
+    {seconds}
+</p>
