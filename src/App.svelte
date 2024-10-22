495164db24d2a9e3dfc22dab983f3cd04ec451dd
moyonenzo <moyon_e@etna-alternance.net>
2024-10-22 14:22:55 +0200

    add sorting & rework on code

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,27 @@
 <script lang="ts">
     import { ModeWatcher } from "mode-watcher";
+    import PersonCounter from "$lib/PersonCounter.svelte";
 
-    import { Separator } from "$lib/components/ui/separator";
-
-    import * as Avatar from "$lib/components/ui/avatar";
-    import * as Card from "$lib/components/ui/card";
-    import Counter from "$lib/Counter.svelte";
+    let counters = [
+        {
+            login: "moyon_e",
+            date: new Date("2024-12-31 12:00:00")
+        },
+        {
+            login: "saconn_a",
+            date: new Date("2024-11-05 17:00:00")
+        },
+        {
+            login: "lequer_r",
+            date: new Date("2025-01-09 17:00:00")
+        }
+    ].sort((a, b) => a.date - b.date);
 </script>
 
 <ModeWatcher />
 
 <div class="flex h-screen flex-col gap-5 justify-center items-center">
-    <Card.Root>
-        <Card.Content>
-            <div class="flex h-5 items-center space-x-4 text-2xl">
-                <Counter date={new Date("2024-11-05 17:00:00")} />
-
-                <Avatar.Root>
-                    <Avatar.Image src="https://trombine.etna.io/saconn_a/large" alt="@moyon_e" />
-                    <Avatar.Fallback>SA</Avatar.Fallback>
-                </Avatar.Root>
-
-                <Separator orientation="vertical" />
-
-                <Avatar.Root>
-                    <Avatar.Image src="https://trombine.etna.io/moyon_e/large" alt="@moyon_e" />
-                    <Avatar.Fallback>ME</Avatar.Fallback>
-                </Avatar.Root>
-
-                <Counter date={new Date("2024-12-31 12:00:00")} />
-            </div>
-        </Card.Content>
-    </Card.Root>
-
-    <Card.Root>
-        <Card.Content>
-            <div class="flex h-5 items-center space-x-4 text-2xl">
-                <Counter date={new Date("2025-01-09 17:00:00")} />
-
-                <Avatar.Root>
-                    <Avatar.Image src="https://trombine.etna.io/lequer_r/large" alt="@moyon_e" />
-                    <Avatar.Fallback>LR</Avatar.Fallback>
-                </Avatar.Root>
-            </div>
-        </Card.Content>
-    </Card.Root>
+    {#each counters as counter}
+        <PersonCounter login={counter.login} date={counter.date} />
+    {/each}
 </div>
---
+++ src/lib/PersonCounter.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Counter from "$lib/Counter.svelte";
+
+    import * as Avatar from "$lib/components/ui/avatar";
+    import * as Card from "$lib/components/ui/card";
+
+    export let login: string;
+    export let date : Date;
+</script>
+
+<Card.Root>
+    <Card.Content>
+        <div class="flex h-5 items-center space-x-4 text-2xl">
+            <Avatar.Root>
+                <Avatar.Image src="https://trombine.etna.io/{login}/large" alt="@{login}" />
+                <Avatar.Fallback />
+            </Avatar.Root>
+
+            <Counter date={date} />
+        </div>
+    </Card.Content>
+</Card.Root>
