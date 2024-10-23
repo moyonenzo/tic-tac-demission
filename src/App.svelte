1155d8da50a3c61b8765b96e89932bab46bb26d7
moyonenzo <moyon_e@etna-alternance.net>
2024-10-23 11:07:53 +0200

    add api connection

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,34 @@
 <script lang="ts">
-    import { ModeWatcher } from "mode-watcher";
-    import PersonCounter from "$lib/PersonCounter.svelte";
+    import axios from "axios";
 
-    let counters = [
-        {
-            login: "moyon_e",
-            date: new Date("2024-12-31 12:00:00")
-        },
-        {
-            login: "saconn_a",
-            date: new Date("2024-11-05 17:00:00")
-        },
-        {
-            login: "lequer_r",
-            date: new Date("2025-01-09 17:00:00")
-        }
-    ].sort((a, b) => a.date - b.date);
+    import { ModeWatcher } from "mode-watcher";
+    import ExclamationTriangle from "svelte-radix/ExclamationTriangle.svelte";
+
+    import PersonCounter from "$lib/PersonCounter.svelte";
+    import * as Alert from "$lib/components/ui/alert/index.js";
+
+    const fetch_counters = async () => {
+        const response = await axios.get("http://85.215.133.119/counters/");
+        return response.data;
+    }
 </script>
 
 <ModeWatcher />
 
 <div class="flex h-screen flex-col gap-5 justify-center items-center">
-    {#each counters as counter}
-        <PersonCounter login={counter.login} date={counter.date} />
-    {/each}
+    {#await fetch_counters()}
+        Chargement ...
+    {:then counters}
+        {#each counters as counter}
+            <PersonCounter login={counter.login} date={new Date(counter.date)} />
+        {/each}
+    {:catch error}
+        <Alert.Root variant="destructive">
+            <ExclamationTriangle class="h-4 w-4" />
+            <Alert.Title>Error</Alert.Title>
+            <Alert.Description>
+                {error}
+            </Alert.Description>
+        </Alert.Root>
+    {/await}
 </div>
